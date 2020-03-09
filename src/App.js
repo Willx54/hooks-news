2040a93fa816ff1e7f8a823db1c887555565bfff
MrStupid <will.priddle@nelsoncroom.co.uk>
2020-03-09 09:14:09 +0000

    Search field. Search button. Clear button.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
 export default function App() {
-  return <div>App</div>;
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("react hooks");
+  const searchInputRef = useRef();
+
+  useEffect(() => {
+    console.log("getResults");
+    getResults();
+  }, []);
+
+  const getResults = async () => {
+    const response = await axios.get(
+      `http://hn.algolia.com/api/v1/search?query=${query}`
+    );
+    setResults(response.data.hits);
+  };
+
+  const handleSearch = event => {
+    event.preventDefault();
+    getResults();
+  };
+
+  const handleClearSearch = () => {
+    setQuery("");
+    searchInputRef.current.focus();
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          onChange={event => setQuery(event.target.value)}
+          value={query}
+          ref={searchInputRef}
+        />
+        <button type="submit">Search</button>
+        <button type="button" onClick={handleClearSearch}>
+          Clear
+        </button>
+      </form>
+      <ul>
+        {results.map(results => (
+          <li key={results.objectID}>
+            <a href={results.url}>{results.title}</a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
