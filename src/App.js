e7f1dc5a73ded50dcd4c2a62bb317b5d1b909947
MrStupid <will.priddle@nelsoncroom.co.uk>
2020-03-10 08:24:05 +0000

    Completed and styled. Tailwindcss bg colours not working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import axios from "axios";
 export default function App() {
   const [results, setResults] = useState([]);
   const [query, setQuery] = useState("react hooks");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const searchInputRef = useRef();
 
   useEffect(() => {
@@ -12,10 +14,17 @@ export default function App() {
   }, []);
 
   const getResults = async () => {
-    const response = await axios.get(
-      `http://hn.algolia.com/api/v1/search?query=${query}`
-    );
-    setResults(response.data.hits);
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `http://hn.algolia.com/api/v1/search?query=${query}`
+      );
+      setResults(response.data.hits);
+    } catch (err) {
+      setError(err);
+    }
+
+    setLoading(false);
   };
 
   const handleSearch = event => {
@@ -29,26 +38,49 @@ export default function App() {
   };
 
   return (
-    <>
-      <form onSubmit={handleSearch}>
+    <div className="container max-w-md mx-auto p-4 m-2 bg-purple-lightest shadow-lg rounded">
+      <img
+        src="https://icon.now.sh/react/c0c"
+        alt="React Logo"
+        className="float-right h-12"
+      />
+      <h1 className="text-grey-darkest font-thin">Hooks News</h1>
+      <form onSubmit={handleSearch} className="mb-2">
         <input
           type="text"
           onChange={event => setQuery(event.target.value)}
           value={query}
           ref={searchInputRef}
+          className="boder p-1 rounded"
         />
-        <button type="submit">Search</button>
-        <button type="button" onClick={handleClearSearch}>
+        <button type="submit" className="bg-orange rounded m-1 p-1">
+          Search
+        </button>
+        <button
+          type="button"
+          onClick={handleClearSearch}
+          className="bg-teal text-white p-1 rounded"
+        >
           Clear
         </button>
       </form>
-      <ul>
-        {results.map(results => (
-          <li key={results.objectID}>
-            <a href={results.url}>{results.title}</a>
-          </li>
-        ))}
-      </ul>
-    </>
+      {loading ? (
+        <div className="font-bold text-orange-dark">Loading results...</div>
+      ) : (
+        <ul className="list-reset leading-normal">
+          {results.map(results => (
+            <li key={results.objectID}>
+              <a
+                href={results.url}
+                className="text-indigo-dark hover:text-indigo-darkest"
+              >
+                {results.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+      {error && <div className="text-red font-bold">{error.message}</div>}
+    </div>
   );
 }
